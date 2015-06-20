f845c04434862185c818ed4d1fbb42c06e7d22bc
Nordrom <wanted.shade@gmail.com>
2015-06-21 01:20:57 +0400

    editing init.js (deleting commentary with broken encoding)

--- js/core/init.js
+++ js/core/init.js
@@ -1,11 +1,7 @@
 var level = 1;
 var points = 0;
 var rules = new Rules;
-function Init(){
-    
-}
-
-//����� ����
+function Init(){}
 Init.prototype.start= function(){
     generator = new Generator();
     generator.run();
